b51610914f93cb9b1a5bfe7382b16f6544a5a056
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-02-24 13:21:34 +0530

    Update main.py

--- apis/image-to-text/main.py
+++ apis/image-to-text/main.py
@@ -23,4 +23,4 @@ async def extract_text(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
